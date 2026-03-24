168a1086bb9a4ee48ef100cd9dcb0e41f463e423
William Ernesto <williamcernestojr@yahoo.com>
2026-03-24 21:00:09 +0800

    fix: move loadFeatures, etc to FeatureLoader.tsx

---
+++ frontend/src/components/FeatureLoader.tsx
@@ -0,0 +1,161 @@
+import { useMap } from "react-leaflet";
+import featuresApi from "@/services/api/features";
+import { useEffect, useRef } from "react";
+import L from "leaflet";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
+
+const FeatureLoader = ({
+	djangoItemsRef,
+	editingLayerRef,
+	setEditingLayer,
+	setLoading,
+}: any) => {
+	const map = useMap();
+	const hasLoaded = useRef(false);
+
+	useEffect(() => {
+		if (!map || !djangoItemsRef.current) return;
+		if (hasLoaded.current) return; // prevent multiple loads
+		hasLoaded.current = true;
+
+		const loadFeatures = async () => {
+			setLoading(true);
+			const data = await featuresApi.fetchAll();
+
+			if (data) {
+				console.log("Data fetched! Loading...");
+
+				L.geoJSON(data as any, {
+					style: (feature: any) => {
+						return feature.properties?.style || {};
+					},
+					pointToLayer: (
+						feature: any,
+						latlng: L.LatLngExpression,
+					) => {
+						return L.circleMarker(
+							latlng,
+							feature.properties?.style || {},
+						);
+					},
+					onEachFeature: (_feature, layer) => {
+						// *******************************
+						// * FOR MAP BOX GEOJSON STYLING *
+						// *******************************
+						const style = _feature.properties.style;
+						_feature.properties["stroke"] = style.color;
+						_feature.properties["stroke-width"] = style.weight;
+						_feature.properties["fill"] = style.fillColor;
+						_feature.properties["fill-opacity"] = style.fillOpacity;
+
+						const popupContent = `
+							<div class="feature-popup-content">
+								<div>
+									<table class="popup-content-table">
+										<tr>
+											<th>ID</th>
+											<td>${_feature.id}</td>
+										</tr>
+										<tr>
+											<th>Name</th>
+											<td>${_feature.properties.name}</td>
+										</tr>
+										<tr>
+											<th>Notes</th>
+											<td>${_feature.properties.notes}</td>
+										</tr>
+										<tr>
+											<th>Created By</th>
+											<td>${_feature.properties.created_by}</td>
+										</tr>
+										<tr>
+											<th>Created At</th>
+											<td>${_feature.properties.created_at}</td>
+										</tr>
+									</table>
+								</div>
+								<div class="controls">
+									<button class="edit">Edit Geometry</button>
+									<button class="delete">Delete</button>
+								</div>
+							</div>
+
+						`;
+						layer.bindPopup(popupContent);
+
+						layer.on("popupopen", () => {
+							const popupEl = layer.getPopup()?.getElement();
+							if (!popupEl) return;
+
+							const deleteBtn = popupEl.querySelector(".delete");
+							deleteBtn?.addEventListener("click", () => {
+								console.log("Delete clicked!");
+								handleDelete(_feature, layer);
+							});
+
+							const editBtn = popupEl.querySelector(".edit");
+							editBtn?.addEventListener("click", () => {
+								const editable = layer as any;
+
+								editable._originalGeoJSON =
+									editable.toGeoJSON();
+
+								if (
+									editingLayerRef.current &&
+									editingLayerRef.current !== editable
+								) {
+									editingLayerRef.current.editing?.disable();
+								}
+
+								editable.editing?.enable();
+
+								editingLayerRef.current = editable;
+								setEditingLayer(editable);
+
+								layer.closePopup();
+							});
+						});
+
+						djangoItemsRef.current?.addLayer(layer);
+					},
+				});
+			}
+
+			setLoading(false);
+		};
+
+		loadFeatures();
+	}, [map]);
+
+	const handleDelete = async (feature: any, layer: any) => {
+		const result = await Swal.fire({
+			title: "Delete feature?",
+			text: "This action cannot be undone.",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonText: "Yes, delete it",
+			cancelButtonText: "Cancel",
+		});
+
+		if (!result.isConfirmed) return;
+
+		const featureId = feature.id;
+
+		const res = await featuresApi.deleteFeature(featureId);
+
+		console.log(res);
+
+		if (res?.success) {
+			djangoItemsRef.current?.removeLayer(layer);
+			toast.success("DELETED!");
+		} else {
+			toast.error(res?.error);
+			console.error("Error deleting feature:", res?.error);
+		}
+	};
+
+	return null;
+};
+
+export default FeatureLoader;
--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -7,6 +7,7 @@ import {
 import ActionBar from "./ActionBar";
 import { useRef, useState, useEffect } from "react";
 import FeatureDialog from "./FeatureDialog";
+import FeatureLoader from "./FeatureLoader";
 import L from "leaflet";
 import "leaflet-draw";
 import { toast } from "react-toastify";
@@ -25,7 +26,7 @@ const Map = () => {
 	const drawnItemsRef = useRef<L.FeatureGroup | null>(null);
 	const djangoItemsRef = useRef<L.FeatureGroup | null>(null);
 
-	const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(false);
 	const [editingLayer, setEditingLayer] = useState<any>(null);
 
 	const refs = {
@@ -34,7 +35,6 @@ const Map = () => {
 		djangoItemsRef: djangoItemsRef,
 	};
 	const editingLayerRef = useRef<any>(null);
-
 
 	const defaultBase = localStorage.getItem("basemap") || "Dark";
 
@@ -80,150 +80,6 @@ const Map = () => {
 		navigate("/login");
 	};
 
-	useEffect(() => {
-		const loadFeatures = async () => {
-
-			const data = await featuresApi.fetchAll();
-
-			if (data) {
-				console.log("Data fetched! Loading...")
-				console.log("LAYERS BEFORE:", djangoItemsRef.current);
-				
-				L.geoJSON(data as any, {
-					style: (feature: any) => {
-						return feature.properties?.style || {};
-					},
-					pointToLayer: (
-						feature: any,
-						latlng: L.LatLngExpression,
-					) => {
-						return L.circleMarker(
-							latlng,
-							feature.properties?.style || {},
-						);
-					},
-					onEachFeature: (_feature, layer) => {
-						// *******************************
-						// * FOR MAP BOX GEOJSON STYLING *
-						// *******************************
-						const style = _feature.properties.style;
-						_feature.properties["stroke"] = style.color;
-						_feature.properties["stroke-width"] = style.weight;
-						_feature.properties["fill"] = style.fillColor;
-						_feature.properties["fill-opacity"] = style.fillOpacity;
-
-						const popupContent = `
-							<div class="feature-popup-content">
-								<div>
-									<table class="popup-content-table">
-										<tr>
-											<th>ID</th>
-											<td>${_feature.id}</td>
-										</tr>
-										<tr>
-											<th>Name</th>
-											<td>${_feature.properties.name}</td>
-										</tr>
-										<tr>
-											<th>Notes</th>
-											<td>${_feature.properties.notes}</td>
-										</tr>
-										<tr>
-											<th>Created By</th>
-											<td>${_feature.properties.created_by}</td>
-										</tr>
-										<tr>
-											<th>Created At</th>
-											<td>${_feature.properties.created_at}</td>
-										</tr>
-									</table>
-								</div>
-								<div class="controls">
-									<button class="edit">Edit Geometry</button>
-									<button class="delete">Delete</button>
-								</div>
-							</div>
-
-						`;
-						layer.bindPopup(popupContent);
-
-						layer.on("popupopen", () => {
-							const popupEl = layer.getPopup()?.getElement();
-							if (!popupEl) return;
-
-							const deleteBtn = popupEl.querySelector(".delete");
-							deleteBtn?.addEventListener("click", () => {
-								console.log("Delete clicked!");
-								handleDelete(_feature, layer);
-							});
-
-							const editBtn = popupEl.querySelector(".edit");
-							editBtn?.addEventListener("click", () => {
-								const editable = layer as any;
-
-								editable._originalGeoJSON =
-									editable.toGeoJSON();
-
-								if (
-									editingLayerRef.current &&
-									editingLayerRef.current !== editable
-								) {
-									editingLayerRef.current.editing?.disable();
-								}
-
-								editable.editing?.enable();
-
-								editingLayerRef.current = editable;
-								setEditingLayer(editable);
-
-								layer.closePopup();
-							});
-						});
-
-						djangoItemsRef.current?.addLayer(layer);
-						console.log("EACHLAYER", djangoItemsRef.current);
-					},
-				});
-
-				console.log("LAYERS AFTER:", djangoItemsRef.current?.getLayers().length);
-				djangoItemsRef.current?.eachLayer((layer) => {
-					console.log(layer);
-				})
-			}
-
-			setLoading(false);
-		};
-
-		loadFeatures();
-	}, []);
-
-	const handleDelete = async (feature: any, layer: any) => {
-		const result = await Swal.fire({
-			title: "Delete feature?",
-			text: "This action cannot be undone.",
-			icon: "warning",
-			showCancelButton: true,
-			confirmButtonText: "Yes, delete it",
-			cancelButtonText: "Cancel",
-		});
-
-		if (!result.isConfirmed) return;
-
-		const featureId = feature.id;
-
-		const res = await featuresApi.deleteFeature(featureId);
-
-		console.log(res);
-
-		if (res?.success) {
-			djangoItemsRef.current?.removeLayer(layer);
-			toast.success("DELETED!");
-		} else {
-			toast.error(res?.error);
-			console.error("Error deleting feature:", res?.error);
-		}
-	};
-
 	const handleCancelEdits = () => {
 		if (!editingLayerRef.current) return;
 
@@ -238,7 +94,6 @@ const Map = () => {
 		if (restored) {
 			if (layer.setLatLngs) {
 				layer.setLatLngs(restored.getLatLngs());
-				// layer.editing.latlngs = restored.getLatLngs();
 			} else if (layer.setLatLng) {
 				layer.setLatLng(restored.getLatLng());
 			}
@@ -272,39 +127,48 @@ const Map = () => {
 		edited.editing.disable();
 	};
 
-	if (loading) return <div>Loading...</div>;
-
-
 	return (
 		<div className="w-full h-screen relative">
 			<MapContainer
-				
 				center={center}
 				zoom={13}
 				zoomControl={false}
 				style={{ height: "100%", width: "100%" }}
 			>
+				{loading && (
+					<div className="absolute inset-0 z-9999 flex items-center justify-center bg-black/40">
+						<span className="text-white text-lg">Loading...</span>
+					</div>
+				)}
+				<FeatureLoader
+					setLoading={setLoading}
+					djangoItemsRef={djangoItemsRef}
+					editingLayerRef={editingLayerRef}
+					setEditingLayer={setEditingLayer}
+				/>
 				<button
 					className="absolute top-2 left-2 z-9999 bg-white/90 p-3 rounded-md cursor-pointer group"
 					onClick={handleLogout}
 				>
 					<LogOut className="transition-transform group-hover:scale-130 duration-300 " />
 				</button>
-
 				{editingLayer && (
 					<div className="absolute right-2 top-16 z-9999 flex flex-col gap-1">
-					<button className=" bg-green-200/90 p-3 rounded-full cursor-pointer group disabled:opacity-60 disabled:cursor-not-allowed"
-					onClick={handleSaveEdits}>
-						<Check className="transition-transform duration-300 group-hover:scale-130 -disabled:active:text-red-700 " />
-					</button>
+						<button
+							className=" bg-green-200/90 p-3 rounded-full cursor-pointer group disabled:opacity-60 disabled:cursor-not-allowed"
+							onClick={handleSaveEdits}
+						>
+							<Check className="transition-transform duration-300 group-hover:scale-130 -disabled:active:text-red-700 " />
+						</button>
 
-					<button className=" bg-red-200/90 p-3 rounded-full cursor-pointer group disabled:opacity-60 disabled:cursor-not-allowed"
-					onClick={handleCancelEdits}>
-						<Ban className="transition-transform duration-300 group-hover:scale-130 -disabled:active:text-red-700 " />
-					</button>
+						<button
+							className=" bg-red-200/90 p-3 rounded-full cursor-pointer group disabled:opacity-60 disabled:cursor-not-allowed"
+							onClick={handleCancelEdits}
+						>
+							<Ban className="transition-transform duration-300 group-hover:scale-130 -disabled:active:text-red-700 " />
+						</button>
 					</div>
 				)}
-
 				<div className="absolute top-2 right-2">
 					<LayersControl position="topright">
 						<BaseLayer checked={defaultBase === "Dark"} name="Dark">
@@ -347,7 +211,6 @@ const Map = () => {
 						</Overlay>
 					</LayersControl>
 				</div>
-
 				<ActionBar
 					refs={refs}
 					openDialog={openDialog}
