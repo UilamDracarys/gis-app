c3d8951b15c84a9a8db3a000e9e654b31a99cc19
William Ernesto <williamcernestojr@yahoo.com>
2026-03-24 15:33:31 +0800

    Update settings.py for faulty path

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -100,7 +100,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://gis-app-production-69a3.up.railway.app/"
 ]
 
 if FRONTEND_URL:
