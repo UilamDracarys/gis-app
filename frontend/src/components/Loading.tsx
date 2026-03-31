33ce9c930d1c63c71453dce374aac8c6478b19b8
William Ernesto <williamcernestojr@yahoo.com>
2026-03-31 11:43:20 +0800

    add animate-bounce to Loading logo

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 const Loading = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-        <img src={logo} width="200px" />
+        <img src={logo} className="w-48 animate-bounce" alt="Loading..." />
     </div>
   )
 }
